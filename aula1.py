56ca4df426efc1c7ae1822ec2b8b6b438f112d9b
Mateusluzsantos1 <mateusluzsantos3@gmail.com>
2024-11-22 10:53:54 -0300

    Primeiro commit

---
+++ aula1.py
@@ -0,0 +1,17 @@
+"""
+Docstring aspas triplas
+para pular linhas 
+com comentários
+OBS:O INTERPRETADOR LER ,DIFERENTE DOS COMENTÁRIOS COM A #
+
+"""
+
+
+# para  fazer o comentário,use a #
+
+
+print("Hello World")
+
+#comentário
+
+print('já sei!')
---
+++ aula2.py
@@ -0,0 +1,10 @@
+print(12, 34, sep='joao' , end='-')
+print(56, 78, sep='')
+print(9, 10, sep='')
+
+#\r \n ->quebra de linha CRLF
+#\n -> LF
+
+
+
+print('expícito', 'é' , 'melhor que implícito', end='-')
